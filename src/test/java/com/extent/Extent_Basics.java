f2c1e4a1e2e7d808e0728d80a9cb8693bfff215a
Shihab Ahmed <shihab4616@gmail.com>
2022-06-18 16:16:31 +0600

    Search Extent report

--- src/test/java/com/extent/Extent_Basics.java
+++ src/test/java/com/extent/Extent_Basics.java
@@ -21,23 +21,23 @@ public class Extent_Basics {
 	public void f1() {
 		
 		//For author name test name and browser name detail's
-		ExtentTest test1= extent.createTest("Launch Browser & Website").assignAuthor("Shihab Ahmed")
+		ExtentTest test= extent.createTest("Launch Browser & Website").assignAuthor("Shihab Ahmed")
 				.assignCategory("Smoke Test").assignDevice("Google Chrome");
 		//for user log test
-		test1.log(Status.PASS, "Launch Browser & Website");
+		test.log(Status.PASS, "Launch Browser & Website");
 		//for user pass test
-		test1.pass("User Launced Website Varified");
+		test.pass("User Launced Website Varified");
 
 	}
     
 	@Test 
 	public void f2() {
-		ExtentTest test1= extent.createTest(" Varify Login").assignAuthor("Shihab Ahmed")
+		ExtentTest test= extent.createTest(" Varify Login").assignAuthor("Asmin Khan")
 				.assignCategory("Sanity Test").assignDevice("Safari");
         //For user Detail's
-		test1.info("  Alert Displaying");
-		test1.pass("User Loged into application");
-		test1.warning("Reset password alart displaying");//FOr warning;
+		test.info("  Alert Displaying");
+		test.pass("User Loged into application");
+		test.warning("Reset password alart displaying");//FOr warning;
 		
 
 
@@ -45,18 +45,18 @@ public class Extent_Basics {
 	
 	@Test 
 	public  void f3() {
-		ExtentTest test1= extent.createTest("Varify Dashboard").assignAuthor("Shihab Ahmed")
+		ExtentTest test= extent.createTest("Varify Dashboard").assignAuthor("Rashiduzzaman")
 				.assignCategory("Business Test").assignDevice("Firefox Driver");
-		test1.skip("varify dashboard skipping");//For skip test; 
+		test.skip("varify dashboard skipping");//For skip test; 
 
 
 	}
 
 	@Test
 	public  void f4() {
-		ExtentTest test1= extent.createTest("Varify user send email from test application").assignAuthor("Shihab Ahmed")
-				.assignCategory("Smoke Test").assignDevice("Edge ");
-		test1.fail("Unable to load reports due to server fail");// If any test case fail;
+		ExtentTest test= extent.createTest("Varify user send email from test application").assignAuthor("Shihab Ahmed")
+				.assignCategory("Functional Test").assignDevice("Edge ");
+		test.fail("Unable to load reports due to server fail");// If any test case fail;
 
 
 	}
@@ -64,9 +64,9 @@ public class Extent_Basics {
 	@Test 
 	public  void f5() {
 		//For author name test naem and browser name detail's
-		ExtentTest test1= extent.createTest("Varify LogOut").assignAuthor("Shihab Ahmed")
-				.assignCategory("Smoke Test").assignDevice("Opera");
-		test1.pass("LogOut is succussful");
+		ExtentTest test= extent.createTest("Varify LogOut").assignAuthor("Asmin Khan")
+				.assignCategory("Regression Test").assignDevice("Opera");
+		test.pass("LogOut is succussful");
 
 	}
 	
@@ -74,7 +74,7 @@ public class Extent_Basics {
 	public  void beforeTest() {
 		
 		extent.attachReporter(spark);
-
+ 
 	}
 	
 	@AfterTest
---
+++ src/test/java/com/extent/SearchTestCase.java
@@ -0,0 +1,64 @@
+package com.extent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchTestCase {
+	 WebDriver driver;
+	
+	ExtentReports extent = new ExtentReports();
+	ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+	
+	
+	@BeforeTest
+	public void browserLaunch() {
+		spark.config().setTheme(Theme.DARK);
+		spark.config().setDocumentTitle("MyReport");
+		extent.attachReporter(spark);
+		
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		extent.flush();
+		driver.quit();
+	}
+	
+	@Test
+	public void TestCase_001() {
+		
+		//for title extent test case
+		ExtentTest test = extent.createTest("Varify My page title").assignAuthor("Shihab").
+				 assignCategory("Functional Test Case").assignDevice("Chrome");
+		
+		test.info("i am capturing the page title");
+		String pagetitle=driver.getTitle();
+		
+		test.info("capture the title as:"+pagetitle);
+		if(pagetitle.equals("google"))
+		{
+			test.pass("page title is varified: Title captured") ;
+		}
+		else
+		{
+			test.fail("paged title is not mathched expected results:"+pagetitle);
+		}
+			
+		
+	}
+
+}
