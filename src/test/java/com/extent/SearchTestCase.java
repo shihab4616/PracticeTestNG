1193bbd4cc89dbb713240edc2e947770df0fb3ac
Shihab Ahmed <shihab4616@gmail.com>
2022-06-19 16:31:19 +0600

    extent log report

--- src/test/java/com/extent/SearchTestCase.java
+++ src/test/java/com/extent/SearchTestCase.java
@@ -8,57 +8,86 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SearchTestCase {
-	 WebDriver driver;
-	
+	WebDriver driver;
 	ExtentReports extent = new ExtentReports();
-	ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
-	
-	 
+	ExtentSparkReporter spark = new ExtentSparkReporter("target/SearchTest.html");
+
+
 	@BeforeTest
 	public void browserLaunch() {
-		spark.config().setTheme(Theme.DARK);
+		spark.config().setTheme(Theme.STANDARD);
 		spark.config().setDocumentTitle("MyReport");
 		extent.attachReporter(spark);
-		
+
 		WebDriverManager.chromedriver().setup();
 		driver= new ChromeDriver();
 		driver.get("https://www.google.com");
-		
+
 	}
-	
-	@AfterTest
-	public void tearDown() {
-		extent.flush();
-		driver.quit();
-	}
-	
+
+
 	@Test
-	public void TestCase_001() {
-		
+	public void Extent_TC_001() {
+
 		//for title extent test case
-		ExtentTest test = extent.createTest("Varify My page title").assignAuthor("Shihab").
-				 assignCategory("Functional Test Case").assignDevice("Chrome");
-		
-		test.info("i am capturing the page title");
+		ExtentTest test1 = extent.createTest("Varify My page title")
+				.assignAuthor("Shihab")
+				.assignAuthor("AsminKhan")
+				.assignCategory("Functional Test Case")
+				.assignDevice("Chrome");
+
+		test1.info("i am capturing the page title");
 		String pagetitle=driver.getTitle();
-		
-		test.info("capture the title as:"+pagetitle);
+
+
+		//use condition for page title matched or not;
+		test1.info("capture the title as:"+pagetitle);
 		if(pagetitle.equals("google"))
 		{
-			test.pass("page title is varified: Title captured") ;
+			test1.pass("page title is varified: Title captured") ;
 		}
 		else
 		{
-			test.fail("paged title is not mathched expected results:");
-		}
-			
+			test1.fail("paged title is not mathched expected results:");
+		}		
+
+	}
+	
+	@Test
+	public void Extent_TC_002() {
+		ExtentTest test2 = extent.createTest("Varify User Email")
+		.assignAuthor("Rashiduzzman")
+		.assignAuthor("Shihab")
+		.assignCategory("Smoke test")
+		.assignDevice("Edge");
 		
+		test2.log(Status.PASS, "You are success to type EMail");
+		test2.info("Email type Success ");
+	}
+	
+	@Test
+	public void Extent_TC_003() {
+		ExtentTest test3 = extent.createTest("Varify LogIn Password")
+		.assignAuthor("Asmin Khan")
+		.assignCategory("Regression test")
+		.assignDevice("FireFox");
+		
+		test3.log(Status.SKIP, "You must be type your password");
+		test3.info("Your pass is wrong");
+	}
+	 
+
+	@AfterTest
+	public void afterTest() {
+		extent.flush();
+		driver.quit();
 	}
 
 }
